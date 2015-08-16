4e67e48493d7a7930cb83008291770623db70ecd
Manoj Nama <manoj.nama@outlook.com>
2015-08-16 11:48:34 +0530

    Updated styling

--- www/js/controllers/dashboard.controller.js
+++ www/js/controllers/dashboard.controller.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module("MEAN")
-  .controller("DashboardCtrl", function () {
+  .controller("DashboardCtrl", function ($scope) {
     var DashboardCtrl= this;
+
+    DashboardCtrl.items = [
+      {id: 1, text: "Bootcamp meeting", time: +new Date(), type: "cal"},
+      {id: 2, text: "JS Channel", time: +new Date(), type: "meet"},
+      {id: 3, text: "CV-GUI Standup", time: +new Date(), type: "standup"},
+      {id: 4, text: "Plugin discussion", time: +new Date(), type: "meet"},
+      {id: 5, text: "Retrospective", time: +new Date(), type: "meet"},
+      {id: 6, text: "Fun team meeting", time: +new Date(), type: "cal"},
+      {id: 7, text: "Growth framework", time: +new Date(), type: "cal"},
+      {id: 8, text: "Conference meet", time: +new Date(), type: "meet"},
+      {id: 9, text: "Nodeschool meeting", time: +new Date(), type: "meet"},
+      {id: 10, text: "Hiring meeting", time: +new Date(), type: "cal"},
+      {id: 11, text: "Hiring meeting", time: +new Date(), type: "cal"},
+      {id: 12, text: "Hiring meeting", time: +new Date(), type: "standup"},
+      {id: 13, text: "Hiring meeting", time: +new Date(), type: "google"},
+      {id: 14, text: "Hiring meeting", time: +new Date(), type: "cal"},
+      {id: 15, text: "Hiring meeting", time: +new Date(), type: "standup"},
+      {id: 16, text: "Hiring meeting", time: +new Date(), type: "meet"},
+      {id: 17, text: "Growth Framework", time: +new Date(), type: "cal"}
+    ];
+
+    $scope.items = DashboardCtrl.items;
+
   });
